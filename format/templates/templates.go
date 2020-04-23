abe51dabd7422affac1ca02a088ea5c819eb4c33
Kamil Dziedzic <kamil@dziedzic.es>
2020-04-23 18:01:38 +0200

    Drop custom and unused functions from template parser.

--- format/templates/templates.go
+++ format/templates/templates.go
@@ -21,33 +21,15 @@ var basicFunctions = template.FuncMap{
 		// Remove the trailing new line added by the encoder
 		return strings.TrimSpace(buf.String())
 	},
-	"split":    strings.Split,
-	"join":     strings.Join,
-	"title":    strings.Title,
-	"lower":    strings.ToLower,
-	"upper":    strings.ToUpper,
-	"pad":      padWithSpace,
-	"truncate": truncateWithLength,
+	"split": strings.Split,
+	"join":  strings.Join,
+	"title": strings.Title,
+	"lower": strings.ToLower,
+	"upper": strings.ToUpper,
 }
 
 // NewParse creates a new tagged template with the basic functions
 // and parses the given format.
 func NewParse(tag, format string) (*template.Template, error) {
 	return template.New(tag).Funcs(basicFunctions).Parse(format)
-}
-
-// padWithSpace adds whitespace to the input if the input is non-empty
-func padWithSpace(source string, prefix, suffix int) string {
-	if source == "" {
-		return source
-	}
-	return strings.Repeat(" ", prefix) + source + strings.Repeat(" ", suffix)
-}
-
-// truncateWithLength truncates the source string up to the length provided by the input
-func truncateWithLength(source string, length int) string {
-	if len(source) < length {
-		return source
-	}
-	return source[:length]
 }
